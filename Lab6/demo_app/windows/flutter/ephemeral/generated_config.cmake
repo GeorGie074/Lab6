ff786aefd773eae76e6da5f2d0af4ecc89f1c1d4
GEORG <pp057105@gmail.com>
2024-09-10 13:05:52 +0700

    Your commit message

---
+++ Lab6/demo_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\cotkg\\AppData\\Local\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\cotkg\\OneDrive\\เดสก์ท็อป\\Lab\\Lab6\\demo_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\cotkg\\AppData\\Local\\flutter"
+  "PROJECT_DIR=C:\\Users\\cotkg\\OneDrive\\เดสก์ท็อป\\Lab\\Lab6\\demo_app"
+  "FLUTTER_ROOT=C:\\Users\\cotkg\\AppData\\Local\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\cotkg\\OneDrive\\เดสก์ท็อป\\Lab\\Lab6\\demo_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\cotkg\\OneDrive\\เดสก์ท็อป\\Lab\\Lab6\\demo_app"
+  "FLUTTER_TARGET=C:\\Users\\cotkg\\OneDrive\\เดสก์ท็อป\\Lab\\Lab6\\demo_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\cotkg\\OneDrive\\เดสก์ท็อป\\Lab\\Lab6\\demo_app\\.dart_tool\\package_config.json"
+)
